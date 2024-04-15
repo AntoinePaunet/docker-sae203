b05d11bbfd2e293de2249dd44b8a54d1c0a93291
Ted Herambert <tedherambert2004@gmail.com>
2024-04-15 14:25:37 +0200

    tmp

--- html/js/Piece.js
+++ html/js/Piece.js
@@ -3,7 +3,6 @@ export default class Piece
 	// attributs "static private"
 	static #NB_LIG = 8;
 	static #NB_COL = 8;
-
 	static #TALLE_IMG = 80;
 	
 	// attributs "protected"
@@ -48,12 +47,5 @@ export default class Piece
 		return true;
 	}
 
-	draw(ctx)
-	{
-		ctx.drawImage(
-			this.image, 
-			(this.#num-1) * Piece.#TALLE_IMG, 0,      Piece.#TALLE_IMG, Piece.#TALLE_IMG, 
-            this.x,                           this.y, Piece.#TALLE_IMG, Piece.#TALLE_IMG
-		);
-	}
+
 }
