277b753df2bbd423bae35633f96ad0be7e807adc
Ted Herambert <tedherambert2004@gmail.com>
2024-04-16 12:32:31 +0200

    modification Piece animation

--- html/js/Piece.js
+++ html/js/Piece.js
@@ -6,8 +6,8 @@ export default class Piece
 		this.x = x;
 		this.y = y;
 		this.vitesse = 0.2;
-		this.xDest = x;
-		this.yDest = y;
+		this.xImg = x;
+		this.yImg = y;
 		
 		this.imgPiece = new Image();
 		this.imgPiece.src = "../images/pieces.png";
@@ -23,7 +23,7 @@ export default class Piece
 	{
 		if(autrePiece === null) return false;
 
-		return (1 <= this.num && this.num <= 6) === (1 <= autrePiece.num && autrePiece.num <= 6) ;
+		return (1 <= this.num && this.num <= 6) === (1 <= autrePiece.num && autrePiece.num <= 6);
 	}
 	
 	getNum () {return this.num;}
@@ -37,28 +37,28 @@ export default class Piece
  
 		tabPieces[this.x][this.y] = null;
 		tabPieces[xDest][yDest] = this;
-		this.xDest = xDest;
-		this.yDest = yDest;
+		this.x = xDest;
+		this.y = yDest;
 		return true;
 	}
 
 	update()
 	{
-		if(this.xDest === this.x && this.yDest === this.y) return true;
+		if(this.xImg === this.x && this.yImg === this.y) return true;
 
-		if(this.xDest > this.x) this.x += this.vitesse;
-		if(this.xDest < this.x) this.x -= this.vitesse;
-		if(this.yDest > this.y) this.y += this.vitesse;
-		if(this.yDest < this.y) this.y -= this.vitesse;
+		if(this.x > this.xImg) this.xImg += this.vitesse;
+		if(this.x < this.xImg) this.xImg -= this.vitesse;
+		if(this.y > this.yImg) this.yImg += this.vitesse;
+		if(this.y < this.yImg) this.yImg -= this.vitesse;
 
-		this.x = Math.round(this.x * 100) / 100;
-		this.y = Math.round(this.y * 100) / 100;
+		this.xImg = Math.round(this.xImg * 100) / 100;
+		this.yImg = Math.round(this.yImg * 100) / 100;
 
 		return false
 	}
 
 	draw(ctx)
 	{
-		ctx.drawImage(this.imgPiece, (this.num-1) * 80, 0, 80, 80, this.x*87.5, this.y*87.5, 87.5, 87.5);
+		ctx.drawImage(this.imgPiece, (this.num-1) * 80, 0, 80, 80, this.xImg*87.5, this.yImg*87.5, 87.5, 87.5);
 	}
 }
