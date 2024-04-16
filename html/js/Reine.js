e677f57001b780ceeb70bdbe35abcab81906d75b
Edwin Michel <edwin.michel@etu.univ-lehavre.fr>
2024-04-16 10:01:14 +0200

    Mouvement Reine

--- html/js/Reine.js
+++ html/js/Reine.js
@@ -8,6 +8,35 @@ export default class Reine extends Piece
     deplacementValide(xDest, yDest, tabPieces)
     {
         return    super.deplacementValide(xDest, yDest, tabPieces) 
-		       && (Math.abs(xDest-this.x) == Math.abs(yDest-this.y) || xDest==this.x || yDest==this.y);
+		       && (Math.abs(xDest-this.x) == Math.abs(yDest-this.y) || xDest==this.x || yDest==this.y)
+               && !this.autrePiece(xDest, yDest, tabPieces);
+    }
+
+    autrePiece(xDest, yDest, tabPieces) 
+    {
+        let dirX = 1;
+        let dirY = 1;
+        if (this.x > xDest) dirX = -1;
+        if (this.y > yDest) dirY = -1;
+    
+        if (yDest === this.y) 
+        {
+            for (let k = 1; k < Math.abs(xDest - this.x); k++) {
+                if (tabPieces[this.x + k * dirX][this.y] !== null) return true;
+            }
+        } 
+        else if (xDest === this.x) 
+        {
+            for (let k = 1; k < Math.abs(yDest - this.y); k++) {
+                if (tabPieces[this.x][this.y + k * dirY] !== null) return true;
+            }
+        } 
+        else 
+        {
+            for (let k = 1; k < Math.abs(xDest - this.x); k++) {
+                if (tabPieces[this.x + k * dirX][this.y + k * dirY] !== null) return true;
+            }
+        }
+        return false;
     }
 }
