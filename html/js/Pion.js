bf96cc74a6d2d2c678ea8a672b4b9a5b81d27a33
Ted Herambert <tedherambert2004@gmail.com>
2024-04-16 15:45:00 +0200

    pion final

--- html/js/Pion.js
+++ html/js/Pion.js
@@ -2,7 +2,7 @@ import Piece from "./Piece.js";
 
 export default class Pion extends Piece
 {
-	constructor(num, x, y) {super(num, x, y); this.xOrig = x; this.yOrig = y;}
+	constructor(num, x, y) {super(num, x, y); this.xOrg = x; this.yOrg = y;}
 
 	deplacementValide(xDest, yDest, tabPieces)
 	{
@@ -17,17 +17,5 @@ export default class Pion extends Piece
 										|| (xDest == this.x + 1 && yDest == this.y - 1 && this.num < 7 && (tabPieces[this.x+1][this.y-1] != null && tabPieces[this.x+1][this.y-1].getNum() >= 7))
 										|| (xDest == this.x - 1 && yDest == this.y - 1 && this.num < 7 && (tabPieces[this.x-1][this.y-1] != null && tabPieces[this.x-1][this.y-1].getNum() >= 7));
 	}
-	/*
-		let dist = 1;
-		if(this.xOrig === this.x && this.yOrig === this.y) dist = 2;
 
-		return    super.deplacementValide(xDest, yDest, tabPieces) 
-			   && !this.autrePiece(xDest, yDest, tabPieces)
-	}
-
-	autrePiece(xDest, yDest, tabPieces)
-	{
-		return tabPieces[xDest, yDest] !== null;
-	}
-	*/
 }
